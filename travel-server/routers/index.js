cae72dfb1ca93a5eed4e0be2b92d00d6f891e634
rkevin-1 <rkevinbramasta@gmail.com>
2024-10-02 23:13:46 +0700

    selesai buat review controller

--- travel-server/routers/index.js
+++ travel-server/routers/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const destinationController = require('../controllers/destinationController');
+const userController = require('../controllers/userController');
 const { auth } = require('../helpers/auth');
 
 const router = express.Router();
 
-router.get('/', destinationController.getAllDestinations);
-router.post('/', auth, destinationController.createDestination);
-router.put('/:id', auth, destinationController.updateDestination);
-router.delete('/:id', auth, destinationController.deleteDestination);
+// Destination routes
+router.get('/destinations', destinationController.getAllDestinations);
+router.post('/destinations', auth, destinationController.createDestination);
+router.put('/destinations/:id', auth, destinationController.updateDestination);
+router.delete('/destinations/:id', auth, destinationController.deleteDestination);
+
+// Auth routes
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+router.post('/logout', auth, userController.logoutUser);
+
+// Protected route to get the user profile
+router.get('/profile', auth, userController.getUserProfile);
 
 module.exports = router;
